8ab5ed4db676aa6790fc14124b98d913796a0d56
maruware <maruware@maruware.com>
2016-05-02 14:51:14 +0900

    refactor

--- lib/endo/cli.rb
+++ lib/endo/cli.rb
@@ -5,8 +5,8 @@ module Endo
   class CLI < Thor
     default_command :exec
 
-    desc "exec usage", "exec desc"
-    def exec(endo_file=nil)
+    desc 'exec usage', 'exec desc'
+    def exec(endo_file = nil)
       if endo_file.nil?
         Dir.glob('endo/*.rb').each do |f|
           exec_proc(f)
@@ -16,12 +16,13 @@ module Endo
       end
     end
 
-    desc "version", "Print version"
+    desc 'version', 'Print version'
     def version
       puts "endo version #{Endo::VERSION}"
     end
 
     private
+
     def exec_proc(file_path)
       executor = Endo::Core.new
       executor.instance_eval File.read(file_path)
