f01986145f30e7c76ecc335d8b72b5efbb9e5308
maruware <maruware@maruware.com>
2016-09-01 14:59:29 +0900

    Update samples

--- endo/sample.rb
+++ endo/sample.rb
@@ -1,57 +1,41 @@
 base_url 'http://localhost:3000'
 basic_auth 'user', 'pass'
 
+email = "#{SecureRandom.hex(8)}@example.com"
+
+post '/users' do
+  param 'email', email
+  param 'password', 'secret'
+end
+
+get '/users/login' do
+  param 'email', email
+  param 'password', 'secret'
+end
+
+post '/articles' do
+  param 'token' do
+    from :get, '/users/login', 'token'
+  end
+  param 'title', 'hello'
+  param 'content', 'Hello, world!'
+end
+
 get '/articles' do
   expect(header: 'Content-Type').to equal 'application/json; charset=utf-8'
 end
 
-post '/articles.json' do
-  param 'article[title]', 'hello'
-  param 'article[content]', 'Hello, world!'
-end
-
-get '/articles/:article_id' do
-  param 'article_id' do
-    from 'post', '/articles.json', 'id'
-    # or
-    # from :post, '/articles.json', -> { self['id'] }
-
-    # or
-    # from :post, '/articles.json' do |r|
-    #   r['id']
-    # end
-  end
-
-  expect(header: 'Content-Type').to equal 'application/json; charset=utf-8'
-  expect(body: 'title').to equal 'hello'
-end
-
-patch '/articles/:article_id.json' do
-  param 'article_id' do
-    from :post, '/articles.json', 'id'
-  end
-
-  param 'article[title]', 'こんにちは'
-end
-
-get '/articles/:article_id' do
-  param 'article_id' do
-    from :post, '/articles.json', 'id'
-  end
-
-  expect(body: 'title').to equal 'こんにちは'
-end
-
-put '/articles/:article_id.json' do
-  param 'article_id' do
-    from :post, '/articles.json', 'id'
-  end
-
-  param 'article[title]', 'bonjour'
-end
-
-delete '/articles/:article_id.json' do
+delete '/articles/:article_id' do
   param :article_id do
-    from :post, '/articles.json', 'id'
+    from :post, '/articles', 'id'
+  end
+  param 'token' do
+    from :get, '/users/login', 'token'
+  end
+end
+
+get '/users/:user_id/articles' do
+  param :user_id do
+    from :get, '/users/login', 'id'
   end
 end
